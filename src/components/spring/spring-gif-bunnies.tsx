426351cf38a8120c25b56e66958751d3e66116bd
sorenta <sorentab.15@gmail.com>
2026-04-03 22:46:35 +0000

    fix(easter): serve gif bunny assets from public for deploy

--- src/components/spring/spring-gif-bunnies.tsx
+++ src/components/spring/spring-gif-bunnies.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import originalBunny from "./original-c5e2239ecc1cc216017604428eb0006f.gif";
-import thumperBunny from "./thumper-faint.gif";
 
 /**
  * Decorative Easter GIF bunnies from the spring asset folder.
@@ -15,7 +13,7 @@ export function SpringGifBunnies() {
         className="pointer-events-none fixed bottom-[calc(env(safe-area-inset-bottom,0px)+98px)] left-3 z-[44] opacity-90"
       >
         <Image
-          src={originalBunny}
+          src="/spring/original-c5e2239ecc1cc216017604428eb0006f.gif"
           alt=""
           unoptimized
           priority
@@ -28,7 +26,7 @@ export function SpringGifBunnies() {
         className="pointer-events-none fixed bottom-[calc(env(safe-area-inset-bottom,0px)+108px)] right-3 z-[44] opacity-80"
       >
         <Image
-          src={thumperBunny}
+          src="/spring/thumper-faint.gif"
           alt=""
           unoptimized
           className="h-auto w-[108px] rounded-2xl shadow-[0_10px_24px_rgba(0,0,0,0.12)]"
